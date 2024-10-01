43997e9d777e39da3b91e4e2659c48317c6206d2
RedietNegash <redietnegash9@gmail.com>
2024-10-01 12:04:20 +0300

    removed cache test

--- app/services/search_engine.py
+++ app/services/search_engine.py
@@ -106,32 +106,3 @@ if __name__ == "__main__":
     print(response1)
 
     
-   import random
-import timeit
-
-def sum_digits(numbers):
-    if numbers not in sum_digits.my_cache:
-        sum_digits.my_cache[numbers] = sum(
-            int(digit) for number in numbers for digit in str(number)
-        )
-    return sum_digits.my_cache[numbers]
-sum_digits.my_cache = {}
-
-numbers = tuple(random.randint(1, 1000) for _ in range(1_000_000))
-
-print(
-    timeit.timeit(
-        "sum_digits(numbers)",
-        globals=globals(),
-        number=1
-    )
-)
-
-print(
-    timeit.timeit(
-        "sum_digits(numbers)",
-        globals=globals(),
-        number=1
-    )
-)
-
