7370d6c2f9c412108e75733674db51a347564393
RedietNegash <redietnegash9@gmail.com>
2024-10-14 09:36:14 +0300

    include user queries in conversation storage

--- app/services/cache.py
+++ app/services/cache.py
@@ -2,7 +2,7 @@ class ConversationCache:
     def __init__(self):
         self.cache = {}
 
-    def add_conversation(self, user_id, session_id, user_message, assistant_message):
+    def add_conversation(self, user_id, session_id, user_query, assistant_message):
         if user_id not in self.cache:
             self.cache[user_id] = {}
         if session_id not in self.cache[user_id]:
@@ -11,7 +11,7 @@ class ConversationCache:
                 "assistant": [] 
             }
    
-        self.cache[user_id][session_id]["user"].append(user_message)
+        self.cache[user_id][session_id]["user"].append(user_query)
         self.cache[user_id][session_id]["assistant"].append(assistant_message)
 
     def get_conversation(self, user_id, session_id):
